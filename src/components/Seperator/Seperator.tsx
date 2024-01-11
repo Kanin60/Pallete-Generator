1f0054564a2a116cb76c0f15baa2cca9b09c8098
Kamilla <113370998+Kanin60@users.noreply.github.com>
2024-01-11 14:54:30 +0100

    Seperator næsten færdig

---
+++ src/components/Seperator/Seperator.tsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+// Det her er et styled component. 
+//Jeg valgte at lave det til et styled component, fordi det er et simpelt component
+export const Seperator = styled.hr`
+    border-image: linear-gradient(to right, rgb(224, 15, 16), rgb(23, 55, 147));
+    border-image-slice: 1;
+    width: 100%;
+    margin: 20px 0;
+`;
+
