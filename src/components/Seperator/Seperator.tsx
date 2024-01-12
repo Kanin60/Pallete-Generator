390c1b1bfdf96e962cd6e2e6d6e229b37b93f2a8
Kamilla <113370998+Kanin60@users.noreply.github.com>
2024-01-12 13:17:15 +0100

    Nav style done

--- src/components/Seperator/Seperator.tsx
+++ src/components/Seperator/Seperator.tsx
@@ -5,6 +5,7 @@ import styled from 'styled-components';
 export const Seperator = styled.hr`
     border-image: linear-gradient(to right, rgb(224, 15, 16), rgb(23, 55, 147));
     border-image-slice: 1;
+    display: block;
     width: 100%;
     margin: 20px 0;
 `;
